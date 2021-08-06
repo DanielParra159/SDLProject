1f556aa784ccbe126872809130310be329a4722a
Daniel Parra <dani.parra.90@gmail.com>
2021-08-06 10:53:33 +0200

    Limit update to 60 fps

--- 3. Improve game loop/Mac/SDLProject/src/main.cpp
+++ 3. Improve game loop/Mac/SDLProject/src/main.cpp
@@ -9,11 +9,20 @@ int main(int argc, char *args[]) {
         game.Init();
 
         while (game.IsRunning()) {
+            double start = SDL_GetTicks();
             game.HandleEvents();
 
             game.Update();
 
             game.Render();
+            const int MS_PER_FRAME = 16;
+            double end = SDL_GetTicks();
+            auto delay = start + MS_PER_FRAME - end;
+
+
+            if (delay > 0) {
+                SDL_Delay(delay);
+            }
         }
 
         game.Release();
